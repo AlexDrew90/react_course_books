bf28a6efa43df64beb28fef623998b53fb67c02f
Alex Drew <alexander.william.drew@gmail.com>
2023-09-01 23:47:56 +0800

    Created random number for ID instead of hard coding it.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ let App = () => {
 
   const createBook = (title) => {
 
-    const updatedBooks = [ ...books, {id: 123, title}];
+    const updatedBooks = [ ...books, {
+      id: Math.round(Math.random() * 9999 ),
+      title}];
     setBooks(updatedBooks);
     console.log(books);
   };
