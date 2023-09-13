425778faabe6157422328505b25264b0c36f08ee
Alex Drew <alexander.william.drew@gmail.com>
2023-09-13 21:17:28 +0800

    Used context with static value.

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,6 +1,10 @@
+import { useContext } from "react";
 import BookShow from "./BookShow";
+import BooksContext from "../context/books";
 
 let BookList = ( {books, onDelete, onEdit} ) => {
+  const value = useContext(BooksContext);
+  console.log(value);
   const renderedBooks = books.map((book) => {
     return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
   });
